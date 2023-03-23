f16ef77dbc0c04cc71947e52d132081fbefa4c10
ZhaneteKlim <janete.klim@gmail.com>
2023-03-23 07:57:59 -1100

    [classroom_13032023]: Add classroom progress

---
+++ src/main/java/lv/acodemy/classroom/Cat.java
@@ -0,0 +1,129 @@
+package lv.acodemy.classroom;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+// it means that for all fields will be getter
+@NoArgsConstructor
+@ToString
+// to create empty constructor
+
+public class Cat {
+    private String name;
+    private String color;
+@Setter    private int age;
+//So Getter wil work for all class, but Setter just for this fields
+@Setter    private double weight;
+    private String breed;
+    private String gender;
+    private boolean hasFur;
+
+    // 5 единиц еды умещается в кота:
+    //private final int max_energy = 5;
+    private int energy = 5;
+
+   // public int getEnergy() {
+   //     return energy;}
+// generate > getter and setter чтобы из приватного значения
+    // сделать и публичное тоже
+    // а чтобы выделить несколько разных строк зажимаем ctrl
+    // составляем конструктор обьекта: generate > constructor
+    // теперь пустого конструктора в классе Кот больше нет,
+    // и кота мы теперь можем создавать только с именем,
+    // или создавать новый конструктор
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    //public String getName() {
+      //  return name;}
+
+    //public String getColor() {
+      //  return color;
+    //}
+
+    //public String getBreed() {
+      //  return breed;
+    //}
+
+    //public String getGender() {
+      //  return gender;
+    //}
+
+    public boolean isHasFur() {
+        return hasFur;
+    }
+
+    //public double getWeight() {
+   //     return weight;
+    //}
+
+ //   public void setWeight(double weight) {
+  //      this.weight = weight;
+    //}
+
+   // public int getAge() {
+   //     return age;
+    //}
+
+    //public void setAge(int age) {
+     //   this.age = age;
+    //}
+
+    public void speak() {
+        System.out.println("Miau!");
+    }
+
+    public void feed () {
+        if (energy == 5) {
+            System.out.println("I am overate");
+    } else {
+            energy++;
+            System.out.println("om-nom-nom!");
+        }
+    // to get energy
+    }
+    public void wolk () {
+        if (energy <= 0) {
+            System.out.println("I don't have energy to walk");
+        } else {
+            System.out.println("tip-top");
+            // energy spending
+            energy = energy - 1;
+            //or/ Energy --;
+        }
+    }
+    // сoздаём пустой конструктор кота generate > select none
+    // generate > constructor > select
+    public Cat(String name, String color, String breed, String gender, boolean hasFur) {
+        this.name = name;
+        this.color = color;
+        this.breed = breed;
+        this.gender = gender;
+        this.hasFur = hasFur;
+    }
+
+  //  public Cat() {
+    //}
+
+//    @Override
+//    public String toString() {
+//        return "Cat{" +
+//                "name='" + name + '\'' +
+//                ", color='" + color + '\'' +
+//                ", age=" + age +
+//                ", weight=" + weight +
+//                ", breed='" + breed + '\'' +
+//                ", gender='" + gender + '\'' +
+//                ", hasFur=" + hasFur +
+//                '}';
+        // правая кнопка мыши generate > to string
+
+
+
+    }
+
---
+++ src/main/java/lv/acodemy/classroom/ObjectExample.java
@@ -0,0 +1,70 @@
+package lv.acodemy.classroom;
+
+public class ObjectExample {
+    public static void main(String[] args) {
+      //  Cat barsik = new Cat();
+      //  System.out.println(barsik.name);
+      //  barsik.name = "barsik";
+      //  System.out.println(barsik.name);
+      //  barsik.name = "barsik";
+      //  barsik.color = "ginger";
+      //  barsik.age = 5;
+      //  barsik.weight = 5;
+      //  barsik.breed = "none";
+      //  barsik.gender = "male";
+      //  barsik.hasFur = true;
+      //  barsik.speak();
+      //  barsik.feed();
+      //  System.out.println(barsik);
+        // после того как мы поделили доступы на паблик и приват
+        // всё стало красным, следующие действия для возврата значений:
+        Cat barsik = new Cat("Barsik");
+        System.out.println(barsik.getAge());
+        System.out.println(barsik);
+
+        barsik.setAge(5);
+        System.out.println(barsik.getAge());
+        //Create getters and setters for : weight and age;
+        //For all others fields create getter method
+barsik.setWeight(6);
+        System.out.println(barsik.getWeight());
+        System.out.println(barsik);
+
+        // 1. crete constructor with name, color, breed, gender, hasFur
+        // 2. create cat with all these field passed into constructor
+
+        //My cat's name is: %s, My cat is %d years old. printf;
+        // %d - numbers, %s for strings,   %,.3f - for double numbers
+        // \n - перенесёт текст на следующую строчку
+        //Создаём нового кота по параметрам.
+        // Чтобы посмотреть параметры, нужно в скобки после Сат счёлкнуть Ctrl+p
+
+        Cat vasja = new Cat("Vasja","ginger", "none","male", true );
+        vasja.setWeight(5.5);
+        vasja.setAge(3);
+        System.out.println(vasja);
+        System.out.printf("My cat's name is: %s, My cat is %d years old.\n", vasja.getName(), vasja.getAge());
+
+        vasja.wolk();
+        vasja.wolk();
+        vasja.wolk();
+        vasja.wolk();
+        vasja.wolk();
+        vasja.wolk();
+        System.out.println(vasja.getEnergy());
+
+        vasja.feed();
+        vasja.feed();
+        vasja.feed();
+        vasja.feed();
+        vasja.feed();
+        vasja.feed();
+        vasja.feed();
+        vasja.feed();
+        System.out.println(vasja.getEnergy());
+
+        // update method feed, so we can feed our cat;
+        // every feed method call incremence energy for 1
+
+    }
+}
